4328e07ffe75c693dbc54aa8614c7654bf9e52ea
Thomas Radloff <bostontrader@gmail.com>
2017-10-25 04:50:11 +0800

    Refactor.

--- app/engine/tradeEngine.js
+++ app/engine/tradeEngine.js
@@ -10,26 +10,28 @@ Engine.prototype.brainWipe = function () {
 }
 
 Engine.prototype.returnTicker =
-  function (markets) { return require('./returnTicker/returnTicker')(markets, this.orders2Buy, this.orders2Sell, this.trades) }
+  function (markets) { return require('./returnTicker/impl')(markets, this.orders2Buy, this.orders2Sell, this.trades) }
 
-Engine.prototype.return24Volume = function () { return require('./return24Volume/return24Volume')(this.trades) }
+Engine.prototype.return24Volume = function () { return require('./return24Volume/impl')(this.trades) }
 
-Engine.prototype.returnOrderBook = function () { return require('./returnOrderBook/returnOrderBook')(this.orders2Buy, this.orders2Sell) }
+Engine.prototype.returnOrderBook = function () { return require('./returnOrderBook/impl')(this.orders2Buy, this.orders2Sell) }
 
-Engine.prototype.returnTradeHistory = function (market) { return require('./returnTradeHistory/returnTradeHistory')(market, this.trades) }
+Engine.prototype.returnTradeHistory = function (market) { return require('./returnTradeHistory/impl')(market, this.trades) }
 
-Engine.prototype.returnChartData = function (market, start, end, period) { return require('./returnChartData/returnChartData')(market, start, end, period, this) }
+Engine.prototype.returnChartData = function (market, start, end, period) { return require('./returnChartData/impl')(market, start, end, period, this) }
 
 Engine.prototype.buy = function (newOrder) {
-  const result = require('./buy/buy')(newOrder, this)
+  const result = require('./buy/impl')(newOrder, this)
   if ('resultingTrades' in result) { this.trades = this.trades.concat(result.resultingTrades) }
   return result
 }
 
 Engine.prototype.sell = function (newOrder) {
-  const result = require('./sell/sell')(newOrder, this)
+  const result = require('./sell/impl')(newOrder, this)
   if ('resultingTrades' in result) { this.trades = this.trades.concat(result.resultingTrades) }
   return result
 }
+
+Engine.prototype.createLoanOffer = function (currency, amount, duration, autoRenew, lendingRate) { return require('./createLoanOffer/impl')(currency, amount, duration, autoRenew, lendingRate, this) }
 
 module.exports = new Engine()
